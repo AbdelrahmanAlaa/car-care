5e126b1de88639b0370305a1b3c70638672520b5
AbdelrahmanAlaa <84046914+AbdelrahmanAlaa@users.noreply.github.com>
2022-03-09 13:11:59 +0200

    updarte img src

--- models/userModel.js
+++ models/userModel.js
@@ -11,7 +11,7 @@ const schema = new mongoose.Schema({
     } ,
        url:{
            type:String,
-           default:'default.png'
+           default:'https://res.cloudinary.com/car-care3/image/upload/v1646093566/on7egnootyrtzxhzrcq3.png'
         } ,
     name: {
         type: String,
