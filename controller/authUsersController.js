85050d907ffe023c8fe0b0cbbe3dec348590b9f3
AbdelrahmanAlaa <84046914+AbdelrahmanAlaa@users.noreply.github.com>
2022-03-10 21:16:29 +0200

    Update authUsersController.js

--- controller/authUsersController.js
+++ controller/authUsersController.js
@@ -150,19 +150,21 @@ exports.forgetPassword =asyncError( async(req,res)=>{
 
 exports.restPassword=async(req,res)=>{
     
-//     const {error}=validateRestPassword(req.body)
-//     if(error)return res.status(404).json({
-//         status:'failed',
-//         message:error.details[0].message
-//     })
+
     const hashedToken=crypto.createHash('sha256').update(req.params.token).digest('hex');
     
     const user = await User.findOne({
          passwordRestToken:hashedToken,
          passwordRestExpires:{$gt:Date.now()}
          });
+         if(!user)return res.status(400).json('Token is invalid or expired ..')
+      
+    const {error}=validateRestPassword(req.body)
+    if(error)return res.status(404).json({
+        status:'failed',
+        message:error.details[0].message
+    })
          
-            if(!user)return res.status(400).json('Token is invalid or expired ..')
             
          const salt = await bcrypt.genSalt(10);
          user.password = await bcrypt.hash(user.password, salt);
