3b0a1597b8d3b407d1e846d67cc8b1d3438e5ba8
AbdelrahmanAla <bidoala73@gmail.com>
2022-07-06 18:31:29 +0200

    lj;ksfsdfa;lkj

--- controller/carSharingControllerInfo.js
+++ controller/carSharingControllerInfo.js
@@ -9,8 +9,8 @@ const upload = require("../middleware/cloudinary");
 const _ = require("lodash");
 const fs = require('fs')
 exports.createCarSharingInfo = asyncError(async (req, res) => {
-  
-  // console.log(req.files)
+  console.log(req.body)
+  console.log(req.files)
   
   req.body.user = req.user._id;
   const checkUser = await CarSharingInfo.findOne({user: req.body.user}) ;
